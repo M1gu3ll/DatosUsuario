4c4c3c23f497eda640d3ea83122559bc60d46c49
Miguel Valverde <mvalverde@righttek.com>
2016-04-27 18:31:12 -0500

    Primer commit

---
+++ app/src/main/java/com/example/mvalverde/datosusuario/ConfirmarDatos.java
@@ -0,0 +1,13 @@
+package com.example.mvalverde.datosusuario;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConfirmarDatos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmar_datos);
+    }
+}
