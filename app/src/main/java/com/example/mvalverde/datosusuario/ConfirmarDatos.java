3622a5f42cb483b77bce2e66e644ff91a14b9569
Miguel Valverde <mvalverde@righttek.com>
2016-05-02 19:02:35 -0500

    Pasar los registros a Confirmar datos

--- app/src/main/java/com/example/mvalverde/datosusuario/ConfirmarDatos.java
+++ app/src/main/java/com/example/mvalverde/datosusuario/ConfirmarDatos.java
@@ -2,12 +2,34 @@ package com.example.mvalverde.datosusuario;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ConfirmarDatos extends AppCompatActivity {
+
+    TextView tvNombre, tvFecha, tvTelefono, tvEmail, tvDescripcion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmar_datos);
+
+        tvNombre = (TextView) findViewById(R.id.tvNombre);
+        tvFecha = (TextView) findViewById(R.id.tvFecha);
+        tvTelefono = (TextView) findViewById(R.id.tvTelefono);
+        tvEmail = (TextView) findViewById(R.id.tvEmail);
+        tvDescripcion = (TextView) findViewById(R.id.tvDescripcion);
+
+        Bundle parametros = getIntent().getExtras();
+        String Nombre = parametros.getString(getResources().getString(R.string.texto_nombre));
+        String Fecha = parametros.getString(getResources().getString(R.string.texto_fecha));
+        String Telefono = parametros.getString(getResources().getString(R.string.texto_telefono));
+        String Email = parametros.getString(getResources().getString(R.string.texto_email));
+        String Descripcion = parametros.getString(getResources().getString(R.string.texto_descripcion));
+
+        tvNombre.setText(Nombre);
+        tvFecha.setText(Fecha);
+        tvTelefono.setText(Telefono);
+        tvEmail.setText(Email);
+        tvDescripcion.setText(Descripcion);
     }
 }
--- app/src/main/java/com/example/mvalverde/datosusuario/IngresoDatos.java
+++ app/src/main/java/com/example/mvalverde/datosusuario/IngresoDatos.java
@@ -1,13 +1,43 @@
 package com.example.mvalverde.datosusuario;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 public class IngresoDatos extends AppCompatActivity {
+
+    Button siguiente;
+    TextView edtNombre, edtTelefono, edtEmail, edtDescripcion;
+    DatePicker dtpFecha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingreso_datos);
+
+        edtNombre = (TextView) findViewById(R.id.edtNombre);
+        dtpFecha = (DatePicker) findViewById(R.id.dtpFecha);
+        edtTelefono = (TextView) findViewById(R.id.edtTelefono);
+        edtEmail = (TextView) findViewById(R.id.edtEmail);
+        edtDescripcion = (TextView) findViewById(R.id.edtDescripcion);
+        siguiente = (Button) findViewById(R.id.btnSiguiente);
+
+        siguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IngresoDatos.this, ConfirmarDatos.class);
+                intent.putExtra(getResources().getString(R.string.texto_nombre), edtNombre.getText().toString());
+                intent.putExtra(getResources().getString(R.string.texto_telefono), edtTelefono.getText().toString());
+                intent.putExtra(getResources().getString(R.string.texto_email), edtEmail.getText().toString());
+                intent.putExtra(getResources().getString(R.string.texto_descripcion), edtDescripcion.getText().toString());
+                intent.putExtra(getResources().getString(R.string.texto_fecha), dtpFecha.getDayOfMonth() + "/" + dtpFecha.getMonth() + "/" + dtpFecha.getYear());
+                startActivity(intent);
+            }
+        });
+
     }
 }
