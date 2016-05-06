2b6efe0427dee8d9a97a4f5a255e2a5e228bcd5e
Miguel Valverde <mvalverde@righttek.com>
2016-05-06 12:28:38 -0500

    Soporte para landscape, idioma en ingles y boton para volver a editar los datos.

--- app/src/main/java/com/example/mvalverde/datosusuario/ConfirmarDatos.java
+++ app/src/main/java/com/example/mvalverde/datosusuario/ConfirmarDatos.java
@@ -2,11 +2,14 @@ package com.example.mvalverde.datosusuario;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ConfirmarDatos extends AppCompatActivity {
 
     TextView tvNombre, tvFecha, tvTelefono, tvEmail, tvDescripcion;
+    Button btnEditar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,8 @@ public class ConfirmarDatos extends AppCompatActivity {
         tvTelefono = (TextView) findViewById(R.id.tvTelefono);
         tvEmail = (TextView) findViewById(R.id.tvEmail);
         tvDescripcion = (TextView) findViewById(R.id.tvDescripcion);
+
+        btnEditar = (Button) findViewById(R.id.btnEditar);
 
         Bundle parametros = getIntent().getExtras();
         String Nombre = parametros.getString(getResources().getString(R.string.texto_nombre));
@@ -31,5 +36,13 @@ public class ConfirmarDatos extends AppCompatActivity {
         tvTelefono.setText(Telefono);
         tvEmail.setText(Email);
         tvDescripcion.setText(Descripcion);
+
+        btnEditar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
